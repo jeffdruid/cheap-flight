b657807ebed6fe13da78db4c89a8ff40eb3a6d40
JeffersonAguiar <jfdruida@gmail.com>
2024-02-17 21:54:17 +0000

    Refact: Refactor web scraping script to iterate through multiple pages

--- run.py
+++ run.py
@@ -1,17 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Send a GET request to the website you want to scrape
-url = "https://books.toscrape.com/catalogue/page-1.html"
-response = requests.get(url)
+current_page = 1
+proceed = True
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+while proceed:
+    # Send a GET request to the website you want to scrape
+    url = "https://books.toscrape.com/catalogue/page-"+str(current_page)+".html"
 
-print(soup.title.text)
+    # Get the HTML content of the webpage
+    response = requests.get(url)
 
-# Find and extract the desired data from the HTML
-# For example, let's extract all the links on the page
-# links = soup.find_all("a")
-# for link in links:
-    # print(link.get("href"))
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Check if the page exists
+    if soup.title.text == "404 Not Found":
+        proceed = False
+    else:
+        print("Scraping page", current_page)
+        current_page += 1
