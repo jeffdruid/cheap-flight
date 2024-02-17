9ce3fccbca151a0eb4481020cc8f0f221f0c704d
JeffersonAguiar <jfdruida@gmail.com>
2024-02-17 21:15:01 +0000

    Feat: Add web scraping test functionality

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+from bs4 import BeautifulSoup
+
+# Send a GET request to the website you want to scrape
+url = "https://books.toscrape.com/catalogue/page-1.html"
+response = requests.get(url)
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+print(soup.title.text)
+
+# Find and extract the desired data from the HTML
+# For example, let's extract all the links on the page
+# links = soup.find_all("a")
+# for link in links:
+    # print(link.get("href"))
