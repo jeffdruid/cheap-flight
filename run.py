4ded10aa51595f152180b578070e70f7d0275922
JeffersonAguiar <jfdruida@gmail.com>
2024-02-17 22:07:27 +0000

    Feat: Added scraping functionality to retrieve book data stock

--- run.py
+++ run.py
@@ -2,9 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 current_page = 1
+data = []
 proceed = True
 
 while proceed:
+    # Print the current page being scraped
+    print(f"Scraping page {current_page}...")
+    
     # Send a GET request to the website you want to scrape
     url = "https://books.toscrape.com/catalogue/page-"+str(current_page)+".html"
 
@@ -18,17 +22,22 @@ while proceed:
     if soup.title.text == "404 Not Found":
         proceed = False
     else:
+        # Find all the books on the page
         all_books = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
         
+        # Loop through each book and get the title, link, price, and stock
         for book in all_books:
             item = {}
+            
             item["title"] = book.find("img").attrs["alt"]
             
             item["link"] = book.find("a").attrs["href"]
             
             item["price"] = book.find("p", class_="price_color").text[1:]
             
-            print(item["price"])
+            item["stock"] = book.find("p", class_="instock availability").text.strip()
+            
+            # Append the data to the list
+            data.append(item)
             
         current_page += 1
-        proceed = False
