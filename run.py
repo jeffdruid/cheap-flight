e2d2840ce344cf6be80996c995ace1c7e121f2cb
JeffersonAguiar <jfdruida@gmail.com>
2024-02-17 22:02:53 +0000

    Refact: Refactored code to scrape and print book details

--- run.py
+++ run.py
@@ -18,5 +18,17 @@ while proceed:
     if soup.title.text == "404 Not Found":
         proceed = False
     else:
-        print("Scraping page", current_page)
+        all_books = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+        
+        for book in all_books:
+            item = {}
+            item["title"] = book.find("img").attrs["alt"]
+            
+            item["link"] = book.find("a").attrs["href"]
+            
+            item["price"] = book.find("p", class_="price_color").text[1:]
+            
+            print(item["price"])
+            
         current_page += 1
+        proceed = False
