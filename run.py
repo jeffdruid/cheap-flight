67d6ef27846bfd380fac55eb4f90a003906cb48a
JeffersonAguiar <jfdruida@gmail.com>
2024-02-20 12:55:18 +0000

    Refact: Refactor scraping code and add main function

--- run.py
+++ run.py
@@ -2,26 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-current_page = 1
-data = []
-proceed = True
-url = input("Enter the URL you want to scrape: \n")
-
-# Check if the URL is allowed to be scraped by robots.txt
-robots_url = url + "/robots.txt"
-robots_response = requests.get(robots_url)
-robots_content = robots_response.text
-
-if "User-agent: *" in robots_content and "Disallow: /" in robots_content:
-    print("This website does not allow scraping.")
-    print("Please check the robots.txt file for more information.")
-    print(robots_url)
-    proceed = False
-else:
-    # If the website allows scraping, proceed with the scraping
+def main():
+    current_page = 1
+    data = []
+    # proceed = True
+    url = input("Enter the URL you want to scrape: \n")
     
+    # Add "https://www." to the URL if it's not already present
+    if not url.startswith("https://www."):
+        url = "https://www." + url
+
+    # Check if the URL is allowed to be scraped by robots.txt
+    # robots_url = url + "/robots.txt"
+    # robots_response = requests.get(robots_url)
+    # robots_content = robots_response.text
+
+    # Check if the website allows scraping
+    # if "User-agent: *" in robots_content and "Disallow: /" in robots_content:
+    #     print("This website does not allow scraping.")
+    #     print("Please check the robots.txt file for more information.")
+    #     print(robots_url)
+    #     proceed = False
+    # else:
+        # If the website allows scraping, proceed with the scraping
+
     # Print the current page being scraped
-    print(f"Scraping page {current_page}...")
+    print(f"Scraping {url}...")
 
     # Get the HTML content of the webpage
     response = requests.get(url)
@@ -31,20 +37,20 @@ else:
 
     # Find all the links on the page
     all_links = soup.find_all("a")
-    
+
     # Loop through all the links and get the href attribute
     for link in all_links:
         # Get the href attribute of the link
         href = link.get("href")
         data.append(href)
-    
+
     # Print the number of links found on the page
-    print(f"Found {len(data)} links on page {current_page}.")
-                    
-# Convert the list to a DataFrame and save it to a CSV file
-df = pd.DataFrame(data, columns=["link"])
-df.to_csv("links.csv", index=False)
-print("Scraping complete!")
+    print(f"Found {len(data)} links on {url}.")
+
+    # Convert the list to a DataFrame and save it to a CSV file
+    df = pd.DataFrame(data, columns=["link"])
+    df.to_csv("links.csv", index=False)
+    print("Scraping complete!")
 
 # Sort the data
 # data.sort()
@@ -64,3 +70,6 @@ print("Scraping complete!")
 # TODO - Add a function to handle status codes
 # TODO - Add a function to handle rate limits
 # TODO - Add a function to handle timeouts
+
+if __name__ == "__main__":
+    main()
